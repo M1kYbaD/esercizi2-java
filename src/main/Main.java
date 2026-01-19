ce428c15f4551f402c1f31a0800c1349b7f203a2
M1kYbaD <167187226+M1kYbaD@users.noreply.github.com>
2026-01-19 11:40:41 +0100

    Refactor Main.java to uncomment method calls for array and matrix operations

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,24 +21,24 @@ public class Main {
 		int[][] matrBase = { { 2, 2, 2 }, { 2, 2, 2 }, { 2, 2, 2 } };
 		int[][] matrEsponente = { { 3, 3, 3 }, { 3, 3, 3 }, { 3, 3, 3 } };
 
-		// Methods.stampArrInt(arrayInt1);
+		Methods.stampArrInt(arrayInt1);
 		Methods.stampArrStr(arrayStr1);
-		// Methods.stampMatrInt(matrInt1);
-		// Methods.stampMatrStr(matrStr1);
-		// Methods.stampMatrStr(matrStr2);
-		// Methods.stampMatrInt(matrBase);
-		// Methods.stampMatrInt(matrEsponente);
+		Methods.stampMatrInt(matrInt1);
+		Methods.stampMatrStr(matrStr1);
+		Methods.stampMatrStr(matrStr2);
+		Methods.stampMatrInt(matrBase);
+		Methods.stampMatrInt(matrEsponente);
 
-		// Methods.arrayIntCresc(arrayInt1);
-		// Methods.arrayIntDecr(arrayInt1);
+		Methods.arrayIntCresc(arrayInt1);
+		Methods.arrayIntDecr(arrayInt1);
 		Methods.arrayStringLungCresc(arrayStr1);
-		// Methods.arrayStringLungDecr(arrayStr1);
-		// System.out.println("La stringa 's' è palindroma: " +
-		// Methods.verStringIsPalindroma(s1));
-		// Methods.verStrIntUguali(matrStr1, matrInt1);
-		// Methods.matrIntPotenze(matrBase, matrEsponente);
-		// System.out.println("Occorrenze 'ciao': " +
-		// Methods.numOccorStrInMatr(matrStr2, "ciao"));
+		Methods.arrayStringLungDecr(arrayStr1);
+		System.out.println("La stringa 's' è palindroma: " +
+				Methods.verStringIsPalindroma(s1));
+		Methods.verStrIntUguali(matrStr1, matrInt1);
+		Methods.matrIntPotenze(matrBase, matrEsponente);
+		System.out.println("Occorrenze 'ciao': " +
+				Methods.numOccorStrInMatr(matrStr2, "ciao"));
 
 	}
 
